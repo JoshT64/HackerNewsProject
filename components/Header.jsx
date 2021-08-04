af1c6764f1de3c62bc094891eed96ab3953c64ed
JoshT64 <joshuatoney55@gmail.com>
2021-08-03 21:15:46 -0400

    Add Header Component

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className='flex justify-between border-b-2 border-blue-400 h-14'>
+      <div className='left ml-6 mt-4'>Left Side</div>
+      <div className='right mr-6 mt-4'>Right Side</div>
+    </header>
+  );
+};
+
+export default Header;
