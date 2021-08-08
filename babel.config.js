41e0476de5508899e98f091b9bb4bbb390295591
JoshT64 <joshuatoney55@gmail.com>
2021-08-07 23:36:10 -0400

    Fix webpack issue

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,3 @@
-// // babel.config.js
-// module.exports = {
-//   presets: [
-//     [
-//       '@babel/preset-env',
-//       '@babel/preset-react',
-//       {
-//         targets: {
-//           esmodules: true,
-//         },
-//       },
-//     ],
-//   ],
-// };
-
 module.exports = {
   presets: [
     [
