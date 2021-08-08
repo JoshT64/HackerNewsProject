6a0e3a50197ce015b071f4e0f2692210b2e6e51e
JoshT64 <joshuatoney55@gmail.com>
2021-08-07 22:47:03 -0400

    Fix webpack issues

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 module.exports = {
   webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
     if (!isServer) {
-      config.node = {
-        resolve: {
-          fallback: {
-            fs: false,
-          },
-        },
-      };
+      config.resolve.fallback.fs = false;
     }
     return config;
   },
