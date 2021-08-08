558fb6ea85a84e30963b6b86b66217aecf09fbac
JoshT64 <joshuatoney55@gmail.com>
2021-08-08 00:34:35 -0400

    Final - fix webpack errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,7 @@
-//a
+module.exports = {
+  extends: 'next',
+  rules: {
+    'react/no-unescaped-entities': 'off',
+    '@next/next/no-page-custom-font': 'off',
+  },
+};
--- setupTests.js
+++
@@ -1,5 +0,0 @@
-import '@testing-library/jest-dom';
-import 'regenerator-runtime/runtime';
-import 'jest';
-import 'babel-jest';
-import '@testing-library/jest-dom';
